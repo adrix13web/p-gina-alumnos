7ca2a812b5fad1517e79ae32e323fa1d79bcc2a6
adrix13web <adrialmacenamiento130@gmail.com>
2025-06-03 09:43:38 -0400

    Create script.js

---
+++ css/js/script.js
@@ -0,0 +1,19 @@
+function saludar() {
+  let nombre = document.getElementById("nombre").value;
+
+  if (nombre.trim() === "") {
+    document.getElementById("mensaje").innerText = "Por favor, escribe tu nombre.";
+  } else {
+    document.getElementById("mensaje").innerText = "¡Hola, " + nombre + "! Bienvenida ✨";
+  }
+}
+
+function cambiarImagen() {
+  let imagen = document.getElementById("imagen");
+
+  if (imagen.src.includes("gatito.png")) {
+    imagen.src = "img/perrito.png";
+  } else {
+    imagen.src = "img/gatito.png";
+  }
+}
